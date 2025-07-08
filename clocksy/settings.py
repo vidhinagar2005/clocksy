4ff232ec7e265d6ee4b5796b5bb9ba9da4633c89
vidhinagar2005 <viddhinagar@gmail.com>
2025-07-08 15:17:37 +0530

    okk

--- clocksy/settings.py
+++ clocksy/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-mp(o3zc=tt5$_ri9b19f*-!^*ry9bb*g$%v3pg0)rne@*ixf$0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
